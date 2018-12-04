6ad94be340471a4f9e748f2124ffad56db4e5b48
Osuology <j.c.r.thegreatrasnicofall@gmail.com>
2018-12-03 21:09:49 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,51 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        print(discord.__version__)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('!she'):
+            guild = client.get_guild(467521350643351566)
+            channel = message.channel
+            member = guild.get_member(message.author.id)
+            sheRole = discord.utils.get(guild.roles, id=469254726911787028)
+            if discord.utils.find(lambda m: m.id == 469254726911787028, member.roles) == None:
+                await member.add_roles(sheRole)
+                await channel.send('I have added the She/Her role to you on Haiku Haven!')
+            else:
+                await member.remove_roles(sheRole)
+                await channel.send('I have removed the She/Her role from you on Haiku Haven!')
+        elif message.content.startswith('!he'):
+            guild = client.get_guild(467521350643351566)
+            channel = message.channel
+            member = guild.get_member(message.author.id)
+            heRole = discord.utils.get(guild.roles, id=469254624155664394)
+            if discord.utils.find(lambda m: m.id == 469254624155664394, member.roles) == None:
+                await member.add_roles(heRole)
+                await channel.send('I have added the He/Him role to you on Haiku Haven!')
+            else:
+                await member.remove_roles(heRole)
+                await channel.send('I have removed the He/Him role from you on Haiku Haven!')
+        elif message.content.startswith('!they'):
+            guild = client.get_guild(467521350643351566)
+            channel = message.channel
+            member = guild.get_member(message.author.id)
+            theyRole = discord.utils.get(guild.roles, id=469254800358506497)
+            if discord.utils.find(lambda m: m.id == 469254800358506497, member.roles) == None:
+                await member.add_roles(theyRole)
+                await channel.send('I have added the They/Them role to you on Haiku Haven!')
+            else:
+                await member.remove_roles(theyRole)
+                await channel.send('I have removed the They/Them role from you on Haiku Haven!')
+        elif message.content.startswith('!help'):
+            channel = message.channel
+            await channel.send('I can give you pronoun roles on Haiku Haven! Just use !he or !she or !they for your role, if you already have that role, I will remove it.')
+
+
+client = MyClient()
+client.run('NDY5MjYxNDEyMTk2Mjg2NDY0.DjFM6A.nI3bhn56F3z0yEpueencffzNvOI')
